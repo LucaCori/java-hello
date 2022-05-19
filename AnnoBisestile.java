768cf13e78091c59e2b38884c86dee536e4f4f71
BertoleroEmanuele <bertoleroemanuele@gmail.com>
2022-05-19 14:33:33 +0200

    Commenti

--- AnnoBisestile.java
+++ AnnoBisestile.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class AnnoBisestile {
 
 	public static void main(String[] args) {
-		int year = 0;
-		Scanner read = new Scanner (System.in);
-		System.out.print("Anno da verificare: ");
-		year = read.nextInt();
-		if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
-			System.out.print("Bisestile!");
+		int year = 0; //anno
+		Scanner read = new Scanner (System.in); //input
+		System.out.print("Anno da verificare: "); //richiesta
+		year = read.nextInt(); //utente
+		if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) { //controllo
+			System.out.print("Bisestile!"); //risposta affermativa
 		}
-		else {
-			System.out.print("Non Bisestile!");
+		else { //altrimenti
+			System.out.print("Non Bisestile!"); //risposta negativa
 		}
 	}
 
